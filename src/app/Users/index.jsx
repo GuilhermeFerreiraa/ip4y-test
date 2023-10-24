e292853f2ce5a8cde2c6dffaca92f8d3fddfc92c
teste-agua-edu <gui.2001@hotmail.com>
2023-10-24 11:19:16 -0300

    feat: add SectionTitle component, reduce mockup test

--- src/app/Users/index.jsx
+++ src/app/Users/index.jsx
@@ -1,81 +1,38 @@
-import { LinearGradient } from "expo-linear-gradient";
 import { View } from "react-native";
-import { TextMedium, TextSemibold } from "~components/Text";
+
+import { LinearGradient } from "expo-linear-gradient";
+
+import SectionTitle from "~components/SectionTitle";
+
+import { TextMedium } from "~components/Text";
+
 import UsersList from "~components/users-list";
 
 export default function UsersScreen() {
   const data = [
     {
-      id: Math.floor().toFixed(2),
-      name: "fulano",
+      id: 1,
+      name: "teste",
       lastName: "da silva",
       email: "teste@teste.com",
       birthdate: "12/12/2002",
       gender: "female",
     },
     {
-      id: Math.floor().toFixed(2),
-      name: "fulano",
-      lastName: "da silva",
+      id: 2,
+      name: "testando",
+      lastName: "da silva joao maria",
       email: "teste@teste.com",
       birthdate: "12/12/2002",
       gender: "male",
     },
     {
-      id: Math.floor().toFixed(2),
-      name: "fulano",
-      lastName: "da silva",
+      id: 3,
+      name: "testado",
+      lastName: "da silva joao teste",
       email: "teste@teste.com",
       birthdate: "12/12/2002",
       gender: "male-female",
-    },
-    {
-      id: Math.floor().toFixed(2),
-      name: "fulano",
-      lastName: "da silva",
-      email: "teste@teste.com",
-      birthdate: "12/12/2002",
-      gender: "male",
-    },
-    {
-      id: Math.floor().toFixed(2),
-      name: "fulano",
-      lastName: "da silva",
-      email: "teste@teste.com",
-      birthdate: "12/12/2002",
-      gender: "male-female",
-    },
-    {
-      id: Math.floor().toFixed(2),
-      name: "fulano",
-      lastName: "da silva",
-      email: "teste@teste.com",
-      birthdate: "12/12/2002",
-      gender: "male",
-    },
-    {
-      id: Math.floor().toFixed(2),
-      name: "fulano",
-      lastName: "da silva",
-      email: "teste@teste.com",
-      birthdate: "12/12/2002",
-      gender: "female",
-    },
-    {
-      id: Math.floor().toFixed(2),
-      name: "fulano",
-      lastName: "da silva",
-      email: "teste@teste.com",
-      birthdate: "12/12/2002",
-      gender: "male-female",
-    },
-    {
-      id: Math.floor().toFixed(2),
-      name: "fulano",
-      lastName: "da silva",
-      email: "teste@teste.com",
-      birthdate: "12/12/2002",
-      gender: "female",
     },
   ];
 
@@ -83,14 +40,22 @@ export default function UsersScreen() {
     <View className="h-full w-full flex-1 items-center justify-start bg-background">
       <View className="px-5 flex-col py-4 items-start justify-start w-full">
         <View className="w-full flex-row justify-between items-center">
-          <TextSemibold textClassName="text-4xl text-white">
-            Lista de Usuários
-          </TextSemibold>
-          {data.length > 0 && (
-            <TextMedium textClassName="text-xs text-white">
-              {data.length} Usuários
-            </TextMedium>
-          )}
+          <SectionTitle text="Registros" style="text-4xl text-white" />
+          <LinearGradient
+            className="h-7 w-[93px] rounded-full border-none items-center justify-center"
+            colors={["#00C59F", "#00D636"]}
+            end={{ x: 0.1, y: 0.2 }}
+          >
+            <View className="w-[95%] items-center h-[90%] px-3 py-1 bg-background rounded-full">
+              {data.length > 0 && (
+                <TextMedium textClassName="text-xs text-white">
+                  {data.length +
+                    " " +
+                    (data.length > 1 ? " usuários" : "usuário")}
+                </TextMedium>
+              )}
+            </View>
+          </LinearGradient>
         </View>
         <LinearGradient
           className="h-[2px] mt-2 w-[65%] border-none items-center justify-center"
