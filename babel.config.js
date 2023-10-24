c83a0eebc9e321930e525efd11ee054c9ba3b93b
teste-agua-edu <gui.2001@hotmail.com>
2023-10-24 02:31:10 -0300

    chore: enruse config is running without any instability

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,40 @@
 module.exports = function (api) {
   api.cache(true);
+  return {};
+};
+
+module.exports = function (api) {
+  api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel", "expo-router/babel"],
+    plugins: [
+      "nativewind/babel",
+      "expo-router/babel",
+      "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@src": "src/",
+            "@assets": "assets/",
+            "@components": "/src/components/",
+            "@helpers": "/src/helpers/",
+            "@services": "/src/services/",
+            "^~(.+)": "./src/\\1",
+          },
+          extensions: [
+            ".ios.js",
+            ".android.js",
+            ".js",
+            ".jsx",
+            ".json",
+            ".tsx",
+            ".ts",
+            ".native.js",
+          ],
+        },
+      ],
+    ],
   };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     "./src/**/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: "#00D636",
+        secondary: "#00C59F",
+        background: "#272424",
+        bg_light: "#0B0B0B",
+        backgroundInherit: "#000000",
+      },
+    },
   },
   plugins: [],
 };
