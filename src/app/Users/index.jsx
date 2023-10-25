0ae39d16a2d5d3874bdf11da97164f4c7c60a5c5
teste-agua-edu <gui.2001@hotmail.com>
2023-10-25 15:44:41 -0300

    feat: add listUsers component

--- src/app/Users/index.jsx
+++ src/app/Users/index.jsx
@@ -2,69 +2,83 @@ import { View } from "react-native";
 
 import { LinearGradient } from "expo-linear-gradient";
 
-import SectionTitle from "~components/SectionTitle";
-
 import { TextMedium } from "~components/Text";
 
-import UsersList from "~components/users-list";
+import GradientLine from "~components/gradient-line";
+
+import SectionTitle from "~components/section-title";
+
+import { baseURL } from "~services/createUser";
+
+import Animated, { FadeInUp, FadeOutDown } from "react-native-reanimated";
+import UserCard from "~components/user-card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function UsersScreen() {
-  const data = [
-    {
-      id: 1,
-      name: "teste",
-      lastName: "da silva",
-      email: "teste@teste.com",
-      birthdate: "12/12/2002",
-      gender: "female",
-    },
-    {
-      id: 2,
-      name: "testando",
-      lastName: "da silva joao maria",
-      email: "teste@teste.com",
-      birthdate: "12/12/2002",
-      gender: "male",
-    },
-    {
-      id: 3,
-      name: "testado",
-      lastName: "da silva joao teste",
-      email: "teste@teste.com",
-      birthdate: "12/12/2002",
-      gender: "male-female",
-    },
-  ];
+  const [users, setUsers] = useState([]);
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleRefresh = async () => {
+    setRefreshing(true);
+
+    const res = await axios.get(`${baseURL}/api/users`);
+    setUsers(res);
+
+    setRefreshing(false);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await axios.get(`${baseURL}/api/users`);
+        setUsers(res.data);
+        console.log(res);
+      } catch (err) {
+        console.error("erro: ", err);
+      }
+    }      
+    fetchData();
+  }, []);
 
   return (
     <View className="h-full w-full flex-1 items-center justify-start bg-background">
       <View className="px-5 flex-col py-4 items-start justify-start w-full">
         <View className="w-full flex-row justify-between items-center">
-          <SectionTitle text="Registros" style="text-4xl text-white" />
+          <SectionTitle text="Registros" classname="text-4xl text-white" />
           <LinearGradient
-            className="h-7 w-[93px] rounded-full border-none items-center justify-center"
+            className="h-7 w-[96px] rounded-full border-none items-center justify-center"
             colors={["#00C59F", "#00D636"]}
             end={{ x: 0.1, y: 0.2 }}
           >
             <View className="w-[95%] items-center h-[90%] px-3 py-1 bg-background rounded-full">
-              {data.length > 0 && (
+              {users.length > 0 && (
                 <TextMedium textClassName="text-xs text-white">
-                  {data.length +
+                  {users.length +
                     " " +
-                    (data.length > 1 ? " usuários" : "usuário")}
+                    (users.length > 1 ? " usuários" : "usuário")}
                 </TextMedium>
               )}
             </View>
           </LinearGradient>
         </View>
-        <LinearGradient
-          className="h-[2px] mt-2 w-[65%] border-none items-center justify-center"
-          colors={["#00C59F", "#00D636"]}
-          end={{ x: 0.1, y: 0.2 }}
-        />
+        <GradientLine />
       </View>
 
-      <UsersList data={data} />
+      <Animated.FlatList
+        horizontal={false}
+        entering={FadeInUp.delay(200)}
+        exiting={FadeOutDown.delay(200)}
+        contentContainerStyle={{ paddingBottom: 24 }}
+        className="w-full h-full p-4 pb-10"
+        keyExtractor={(item, id) => id.toString()}
+        data={users}
+        renderItem={({ item }) => <UserCard data={item} />}
+        showsVerticalScrollIndicator={false}
+        refreshing={refreshing}
+        onRefresh={handleRefresh}
+      />
     </View>
   );
 }
